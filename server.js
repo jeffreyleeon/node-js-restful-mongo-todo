1749ff8df3e9574117ca10bf094141deffda87f5
Lee Chun On <jeffreyleeon@gmail.com>
2017-07-05 21:01:57 +0800

    Update server.js to start the server with controller, route and model

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Task = require('./api/models/todoListModel');
 var app = express();
 var port = process.env.PORT || 3000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/restful-todo');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var taskRoutes = require('./api/routes/todoListRoutes');
+taskRoutes(app);
 
 app.listen(port, () => console.log('todo list RESTful API server started on: ' + port));
