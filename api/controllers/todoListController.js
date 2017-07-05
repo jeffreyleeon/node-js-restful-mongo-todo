fcc87a45dd236aa7798194a7065155f92658ca52
Lee Chun On <jeffreyleeon@gmail.com>
2017-07-05 19:19:02 +0800

    Add a controller

---
+++ api/controllers/todoListController.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Task = mongoose.model('Tasks');
+
+var listAllTasks = (req, res) => {
+    Task.find({}, (err, task) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(task);
+    });
+};
+
+var createATask = (req, res) => {
+    var newTask = new Task(req.body);
+    newTask.save((err, task) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(task);
+    });
+};
+
+var readATask = (req, res) => {
+    Task.findById(req.params.taskId, (err, task) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(task);
+    });
+};
+
+var updateATask = (req, res) => {
+    Task.findOneAndUpdate(
+        {_id: req.params.taskId},
+        req.body,
+        {new: true},
+        (err, task) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(task);
+        }
+    );
+};
+
+var deleteATask = (req, res) => {
+    Task.remove({
+        _id: req.params.taskId,
+    }, (err, task) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Task successfully deleted' });
+    });
+};
+
+module.exports = {
+    listAllTasks,
+    createATask,
+    readATask,
+    updateATask,
+    deleteATask,
+};
