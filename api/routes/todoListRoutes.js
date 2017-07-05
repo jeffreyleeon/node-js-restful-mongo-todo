0e9568a61b3e66ab06b81443051e2726b0ba24de
Lee Chun On <jeffreyleeon@gmail.com>
2017-07-05 19:11:31 +0800

    Add todo list route

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(app) {
+    var todoList = require('../controllers/todoListController');
+
+    // todoList Routes
+    app.route('/tasks')
+        .get(todoList.listAllTasks)
+        .post(todoList.createATask);
+
+    app.route('/tasks/:taskId')
+        .get(todoList.readATask)
+        .put(todoList.updateATask)
+        .delete(todoList.deleteATask);
+
+};
